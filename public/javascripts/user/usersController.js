70a73fccb9f32053d4d0cc5b649ab174dd2bb775
Benoit Orihuela <bor@zenexity.com>
2013-07-26 11:48:41 +0200

    First rough version of users list

--- public/javascripts/user/usersController.js
+++ public/javascripts/user/usersController.js
@@ -1,3 +1,13 @@
 app.controller("UsersCtrl", ["$scope", function ($scope) {
-  
+
+    $scope.users = [
+        { _id : "51f23e903dfb9380006ce169", profile : 
+            { id : "107176102057999375218", email : "bor@zenexity.com", verifiedEmail : "true", 
+              name : "Benoit Orihuela", link : "https://plus.google.com/107176102057999375218", 
+              gender : "male", birthday : "1976-01-23", locale : "fr"}},
+        { _id : "002", profile : 
+            { id : "002", email : "pdi@zenexity.com", verifiedEmail : "true", 
+              name : "Paul Dijou", link : "https://plus.google.com/002", 
+              gender : "male", birthday : "1986-01-23", locale : "fr"}}
+    ]
 }]);
