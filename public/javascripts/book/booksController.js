02e4fc58cc25f4bb80e7d71fe5941f563d37aace
ybr <ybr@zenexity.com>
2013-07-26 12:55:49 +0200

    Plug search on OpenLibrary

--- public/javascripts/book/booksController.js
+++ public/javascripts/book/booksController.js
@@ -1,6 +1,15 @@
 app.controller("BooksCtrl", ["$scope", "$location", "$stateParams", "Books", function ($scope, $location, $stateParams, booksService) {
   if($stateParams.pattern) {
-    $scope.books = booksService.search($stateParams.pattern)
+    booksService.search($stateParams.pattern).then(function(book) {
+      console.log(book);
+        $scope.books =  [{
+          isbn: $stateParams.pattern,
+          titre: book.title,
+          resume: "",
+          imageUrl: book.cover.medium,
+          authors: book.authors.map(function(a) { return a.name })
+        }]
+    })
   }
 
   $scope.add = function(isbn) {
--- public/javascripts/book/booksService.js
+++ public/javascripts/book/booksService.js
@@ -1,21 +1,6 @@
 app.factory("Books", ["Restangular", function (Restangular) {
   function search(pattern) {
-    return [{
-      isbn: "1234",
-      titre: "Bilbo le Hobbit",
-      resume: "Super Saga",
-      imageUrl: "http://static.decitre.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/9/7/8/2/0/1/2/0/9782012035614FS.gif"
-    }, {
-      isbn: "1234",
-      titre: "Bilbo le Hobbit",
-      resume: "Super Saga",
-      imageUrl: "http://static.decitre.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/9/7/8/2/0/1/2/0/9782012035614FS.gif"
-    }, {
-      isbn: "1234",
-      titre: "Bilbo le Hobbit",
-      resume: "Super Saga",
-      imageUrl: "http://static.decitre.fr/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/9/7/8/2/0/1/2/0/9782012035614FS.gif"
-    }]
+    return Restangular.one("test/openlibrary", "ISBN:" + pattern).doGET()
   }
 
   function list () {
